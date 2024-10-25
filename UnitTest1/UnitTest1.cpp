bd1fba81c789716e16a0c03869083a8542bc89d4
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-25 18:18:34 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab8.1(char).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestCountFunction)
+		{
+			char str[] = "a+b-c=d++e--f==";
+			int countPlus = 0, countMinus = 0, countEqual = 0;
+
+			Count(str, countPlus, countMinus, countEqual);
+
+			Assert::AreEqual(3, countPlus);   // Має бути 3 символи '+'
+			Assert::AreEqual(3, countMinus);  // Має бути 3 символи '-'
+			Assert::AreEqual(3, countEqual);  // Має бути 3 символи '='
+		}
+
+		TEST_METHOD(TestChangeFunction)
+		{
+			char str[] = "a+b-c==d++e--f==";
+			char expected[] = "a+b-c***d***e***f***";
+
+			char* result = Change(str);
+
+			Assert::AreEqual(expected, result); // Перевіряємо, чи результат співпадає з очікуваним
+
+			delete[] result; // Видаляємо динамічну пам'ять, виділену у функції Change
+		}
+	};
+}
---
+++ lab8.1(char).cpp
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void Count(char* s, int& countPlus, int& countMinus, int& countEqual)
+{
+	countPlus = 0;
+	countMinus = 0;
+	countEqual = 0;
+
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '+')
+			countPlus++;
+		else if (s[i] == '-')
+			countMinus++;
+		else if (s[i] == '=')
+			countEqual++;
+	}
+}
+
+char* Change(char* s)
+{
+	char* t = new char[strlen(s) * 2]; // Виділяю достатньо пам'яті для можливих змін
+	int pos1 = 0, pos2 = 0;
+	*t = 0;
+
+	while (s[pos1] != '\0')
+	{
+		if ((s[pos1] == '+' && s[pos1 + 1] == '+') ||
+			(s[pos1] == '-' && s[pos1 + 1] == '-') ||
+			(s[pos1] == '=' && s[pos1 + 1] == '='))
+		{
+			strcat(t, "***");
+			pos1 += 2; // Пропускаємо пару символів
+		}
+		else
+		{
+			strncat(t, s + pos1, 1);
+			pos1++;
+		}
+	}
+	strcpy(s, t);
+	return t;
+}
+
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+
+	int countPlus, countMinus, countEqual;
+	Count(str, countPlus, countMinus, countEqual);
+
+	cout << "Number of '+' symbols: " << countPlus << endl;
+	cout << "Number of '-' symbols: " << countMinus << endl;
+	cout << "Number of '=' symbols: " << countEqual << endl;
+
+	char* dest = new char[151];
+	dest = Change(str);
+
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+
+	return 0;
+}
